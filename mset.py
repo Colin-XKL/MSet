c73f32fab81dcc1dcf01f331d7fa25e3a35039e3
Colin <Colin_XKL@Outlook.com>
2021-07-01 23:52:23 +0800

    add mset

--- mset.py
+++ mset.py
@@ -1,20 +1,53 @@
 from shutil import which
 import os
-commands={}
 
-def installed(name:str):
+commands = {}
+
+
+def installed(name: str):
     return which(name) is not None
 
-def new_command_map(cmd:list,func):
-    global commands
-    for k in cmd:
-        commands[k]=func
 
-def pypi():
-    return os.system('pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple')
-    
-if __name__=='__main__':
+def checkcmd(cmd: str):
+    def wrapper_out(func):
+        def wrapper(*args, **kwargs):
+            if installed(cmd):
+                func(*args, **kwargs)
+            else:
+                print("ERR: " + cmd + " 未安装")
+                return True  # the os.system zero when succeed
+
+        return wrapper
+
+    return wrapper_out
+
+
+class Mset:
+    def new_command_map(self, cmd: list, func):
+        global commands
+        for k in cmd:
+            commands[k] = func
+
+    @checkcmd("pip")
+    def pypi(self):
+        return os.system(
+            "pip config set global.index-url http://mirrors.cloud.tencent.com/pypi/simple/"
+        )
+
+    @checkcmd("npm")
+    def npm(self):
+        return os.system(
+            "npm config set registry https://mirrors.cloud.tencent.com/npm/"
+        )
+
+    @checkcmd("test")
+    def test(self):
+        return os.system("echo hello")
+
+
+if __name__ == "__main__":
     print("MSet")
-    failed=pypi()
+    m = Mset()
+    failed = m.pypi()
     if failed:
-        print('ooops')
+        print("ooops")
